354608e4e66ab94c26e308469dda247b3d2837b6
simon spagnol <s.spagnol@aims.gov.au>
2018-08-15 18:03:48 +1000

    Update readECOraw.m to handle single data point in burst

--- Parser/readECOraw.m
+++ Parser/readECOraw.m
@@ -197,9 +197,11 @@ if nBurst > 1
     durationBurst = zeros(nBurst, 1);
     for i=1:nBurst
         timeBurst = time(iBurst(i):iBurst(i+1)-1);
-        sampleIntervalInBurst(i) = median(diff(timeBurst*24*3600));
-        firstTimeBurst(i) = timeBurst(1);
-        durationBurst(i) = (timeBurst(end) - timeBurst(1))*24*3600 + sampleIntervalInBurst(i);
+        if numel(timeBurst)>1 % deals with the case of a file with a single sample in a single burst
+            sampleIntervalInBurst(i) = median(diff(timeBurst*24*3600));
+            firstTimeBurst(i) = timeBurst(1);
+            durationBurst(i) = (timeBurst(end) - timeBurst(1))*24*3600 + sampleIntervalInBurst(i);
+        end
     end
     
     iNaNBurst = isnan(sampleIntervalInBurst); % it is possible that only one sample is output for a burst
