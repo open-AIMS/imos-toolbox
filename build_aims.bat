27700cd717a626e58d10410e12a46f7dccccb5ac
Simon Spagnol <s.spagnol@mail.com>
2021-06-11 09:46:16 +0800

    Update to master which has updates for RDI ADCP issues.

--- build_aims.bat
+++ build_aims.bat
@@ -3,9 +3,12 @@ set CONDA=%USERPROFILE%\AppData\Local\Continuum\Anaconda3\condabin\conda
 call %CONDA% activate "%USERPROFILE%\AppData\Local\Continuum\Anaconda3"
 
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_291
-set ANT_HOME=c:\opt\apache-ant-1.10.9
+set ANT_HOME=c:\opt\apache-ant-1.10.10
 set MATLAB_ROOT="C:\Program Files\MATLAB\R2018b"
 
 SET PATH=%ANT_HOME%\bin;%MATLAB_ROOT%\bin;%JAVA_HOME%\bin;%PATH%
 
-python build_aims.py --arch=win64 --mat_path=%MATLAB_ROOT%\bin\matlab
+rem python build_aims.py --arch=win64 --mat_path=%MATLAB_ROOT%\bin\matlab
+rem 
+rem python build.py --arch=win64 --matlab_path=%MATLAB_ROOT%\bin\matlab --mcc_path=%MATLAB_ROOT%\bin\matlab
+python build.py --arch=win64
