76815a7ac64a5d29a7c03393bdfcdfea76c8dd4d
Simon Spagnol <s.spagnol@mail.com>
2021-08-19 14:44:24 +0800

    Update global_attributes_timeSeries.txt to use setAIMSmetadata.m for more attributes.

--- Util/setAIMSmetadata.m
+++ Util/setAIMSmetadata.m
@@ -2,13 +2,13 @@ function metadataStr = setAIMSmetadata(site,metadataField)
 %This function allows a generic config file to be used with the
 %IMOS toolbox for processing AIMS data.
 %The function call [mat setAIMSmetadata('[ddb Site]','naming_authority')]
-%in global_attributes.txt calls this function with the site and the
+%in global_attributes_timeSeries.txt calls this function with the site and the
 %datafield as arguements. This function then passes back an appropriate
 %string for that site and field.
-%If the site is not reconised, default "IMOS" strings are returned.
+%If the site is not reconised, default strings are returned.
 
 %%
-% Site Names
+% IMOS Site Names
 % NRS : National Reference Stations eg
 %   NRSDAR : Darwin
 %   NRSYON : Yongala
@@ -17,65 +17,27 @@ function metadataStr = setAIMSmetadata(site,metadataField)
 % PIL : Pilbara mooring array
 % KIM : Kimberley mooring array
 % NIN : Ningaloo Reef Tantabiddi 50m mooring (IMOS)
+% GBR : GBR mooring array
 % TAN : Ningaloo Reef Tantabiddi 100m  mooring (AIMS)
-% SCR : RV Falkor cruise FK150410 (AIMS 6204) at Scott Reef and surrounds
 % CAM : Camden Sound moorings CAM050/CAM100 Aug-2014 -- Jul-2015
-% TAN100 : Jul-2010 onwards
 %
+% Other Site Names
+% SCR : RV Falkor cruise FK150410 (AIMS 6204) at Scott Reef and surrounds
 
 %%
-switch metadataField
-    % project
-    case 'project'
-        metadataStr = getAIMSmetadata_project(site);
-        return
+dbsite = executeQuery( 'Sites', 'Site', site);
+% if the Research Activity start with IMOS then tada it's IMOS
+isIMOS = ~isempty(regexp(dbsite.ResearchActivity, '^IMOS'));
 
-        % institution
-    case 'institution'
-        metadataStr = getAIMSmetadata_institution(site);
-        return
-
-        % abstract
-    case 'abstract'
-        metadataStr = getAIMSmetadata_abstract(site);
-        return
-        
-        % references
-    case 'references'
-        metadataStr = getAIMSmetadata_references(site);
-        return
-        
-        % principal_investigator
-    case 'principal_investigator'
-        metadataStr = getAIMSmetadata_principal_investigator(site);
-        return
-
-        % principal_investigator_email
-    case 'principal_investigator_email'
-        metadataStr = getAIMSmetadata_principal_investigator_email(site);
-        return
-        
-        % institution_references
-    case 'institution_references'
-        metadataStr = getAIMSmetadata_institution_references(site);
-        return
-        
-        % acknowledgement, imosToolbox from v2.5 onwards
-    case 'acknowledgement'
-        metadataStr = getAIMSmetadata_acknowledgement(site);
-        return
-        
-        % project_acknowledgement, imosToolbox to v2.4
-    case 'project_acknowledgement'
-        metadataStr = getAIMSmetadata_project_acknowledgement(site);
-        return
-        
-        % local_time_zone
-    case 'local_time_zone'
-        metadataStr = getAIMSmetadata_local_time_zone(site);
-        return
-        
+try
+    fnh = str2func(['getAIMSmetadata_' metadataField]);
+    metadataStr = fnh(site, dbsite, isIMOS);
+catch e
+    disp(e);
+    warning(['Unknown metadata field : ' metadataField]);
+    metadataStr = 'UNKNOWN';
 end
+
 end
 
 %%
@@ -87,86 +49,113 @@ end
 
 %%
 function [sitename, yy, mm] = splitSite(site)
-    strsplits = split(site,'-');
-    sitename = strsplits{1};
-    [yy,mm] = datevec(strsplits{2},'yymm');
-    
-end
-%%
-function metadataStr = getAIMSmetadata_project(site)
+strsplits = split(site,'-');
+sitename = strsplits{1};
+[yy,mm] = datevec(strsplits{2},'yymm');
 
-if identifySite(site,{'SCR'})
-    metadataStr = 'Timor Sea Reef Connections';
-elseif identifySite(site,{'CAM', 'TAN'})
-    % current AODN ingest isn't quite setup for not IMOS data yet. Being looked at.
-    %metadataStr = 'AIMS';
-    metadataStr = 'Integrated Marine Observing System (IMOS)';
+end
+
+%%
+function metadataStr = getAIMSmetadata_naming_authority(site, dbsite, isIMOS)
+
+if isIMOS
+    metadataStr = 'IMOS';
 else
-    metadataStr = 'Integrated Marine Observing System (IMOS)';
-end
-
-end
-
-%%
-function metadataStr = getAIMSmetadata_institution(site)
-
-if identifySite(site,{'NRS'})
-    metadataStr = 'ANMN-NRS';
-elseif identifySite(site,{'SCR'})
     metadataStr = 'AIMS';
-elseif identifySite(site,{'CAM', 'TAN'})
-    % current AODN ingest isn't quite setup for not IMOS data yet. Being looked at.
-    %metadataStr = 'AIMS';
-    metadataStr = 'ANMN-QLD';
-else
-    metadataStr = 'ANMN-QLD';
 end
 
 end
 
 %%
-function metadataStr = getAIMSmetadata_abstract(site)
+function metadataStr = getAIMSmetadata_project(site, dbsite, isIMOS)
 
-if identifySite(site,{'SCR'})
-    metadataStr = ['About halfway between Northwestern Australia ',...
-        'and Indonesia lie some of the planet''s most remote and ',...
-        'healthy coral reefs, with biodiversity in places rivaling ',...
-        'that of the much better known Great Barrier Reef. Yet the ',...
-        'physical connections between these reefs, the factors ',...
-        'responsible for their health, and the conditions most ',...
-        'likely to threaten them are not fully understood. In April 2015, ',...
-        'the RV Falkor steamed to the region for a collaborative ',...
-        'project aimed at exploring these connections. The work ',...
-        'includes expanding on previous research at shallower reefs, ',...
-        'as well as the first ever exploration of some deeper sites.'];
+if isIMOS
+    metadataStr = 'Integrated Marine Observing System (IMOS)';
 else
+    if identifySite(site,{'^SCR'})
+        metadataStr = 'Timor Sea Reef Connections';
+    elseif ~isempty(dbsite.ResearchActivity)
+        metadataStr = ['AIMS : ' strtrim(dbsite.ResearchActivity)];
+    else
+        metadataStr = 'AIMS';
+    end
+end
+
+end
+
+%%
+function metadataStr = getAIMSmetadata_institution(site, dbsite, isIMOS)
+
+if isIMOS
+    if contains(site, {'NRS'})
+        metadataStr = 'ANMN-NRS';
+    else
+        metadataStr = 'ANMN-QLD';
+    end
+else
+    metadataStr = 'AIMS-QLD';
+end
+
+end
+
+%%
+function metadataStr = getAIMSmetadata_abstract(site, dbsite, isIMOS)
+
+if isIMOS
     metadataStr = ['The Queensland and Northern Australia mooring ',...
         'sub-facility is operated by the Australian Institute of Marine ',...
         'Science.  The sub-facility is responsible for ',...
         'moorings in two geographic regions: Queensland''s Great Barrier ',...
         'Reef and Northern Australia; ',...
         'where National Reference Stations and a number of regional moorings are maintained.'];
-end
-
-end
-
-%%
-function metadataStr = getAIMSmetadata_references(site)
-
-if identifySite(site,{'SCR'})
-    metadataStr = 'http://data.aims.gov.au/, http://www.schmidtocean.org/story/show/3493';
 else
-    metadataStr = 'http://imos.org.au, http://www.aims.gov.au/imosmoorings/';
+    if identifySite(site,{'SCR'})
+        metadataStr = ['About halfway between Northwestern Australia ',...
+            'and Indonesia lie some of the planet''s most remote and ',...
+            'healthy coral reefs, with biodiversity in places rivaling ',...
+            'that of the much better known Great Barrier Reef. Yet the ',...
+            'physical connections between these reefs, the factors ',...
+            'responsible for their health, and the conditions most ',...
+            'likely to threaten them are not fully understood. In April 2015, ',...
+            'the RV Falkor steamed to the region for a collaborative ',...
+            'project aimed at exploring these connections. The work ',...
+            'includes expanding on previous research at shallower reefs, ',...
+            'as well as the first ever exploration of some deeper sites.'];
+    else
+        metadataStr = ['The Australian Institute of Marine Science (AIMS) ',...
+            'is Australia’s tropical marine research agency. We play a ',...
+            'pivotal role in providing large-scale, long-term and ',...
+            'world-class research that helps governments, industry and ',...
+            'the wider community to make informed decisions about the ',...
+            'management of Australia’s marine estate. AIMS is a ',...
+            'Commonwealth statutory authority established by the ',...
+            'Australian Institute of Marine Science Act 1972.'];
+    end
 end
 
 end
 
 %%
-function metadataStr = getAIMSmetadata_principal_investigator(site)
+function metadataStr = getAIMSmetadata_references(site, dbsite, isIMOS)
 
-if identifySite(site,{'GBR','NRSYON'})
+if isIMOS
+    metadataStr = 'http://imos.org.au, http://www.aims.gov.au/imosmoorings/';
+else
+    if identifySite(site,{'SCR'})
+        metadataStr = 'http://data.aims.gov.au/, http://www.schmidtocean.org/story/show/3493';
+    else
+        metadataStr = 'http://data.aims.gov.au/';
+    end
+end
+
+end
+
+%%
+function metadataStr = getAIMSmetadata_principal_investigator(site, dbsite, isIMOS)
+
+if identifySite(site,{'^GBR','NRSYON'})
     metadataStr = 'AIMS, Q-IMOS';
-elseif identifySite(site,{'ITF','PIL', 'KIM', 'NIN'})
+elseif identifySite(site,{'ITF','PIL', 'KIM', 'NIN', 'NWS'})
     metadataStr = 'AIMS, WAIMOS';
 elseif identifySite(site,{'NRSDAR'})
     metadataStr = 'IMOS';
@@ -181,16 +170,16 @@ end
 end
 
 %%
-function metadataStr = getAIMSmetadata_principal_investigator_email(site)
+function metadataStr = getAIMSmetadata_principal_investigator_email(site, dbsite, isIMOS)
 
-if identifySite(site,{'GBR','NRSYON'})
-    metadataStr = 'c.steinberg@aims.gov.au';
-elseif identifySite(site,{'ITF','PIL', 'KIM', 'NIN'})
-    metadataStr = 'c.steinberg@aims.gov.au';
+if identifySite(site,{'^GBR','NRSYON'})
+    metadataStr = 'm.cahill@aims.gov.au';
+elseif identifySite(site,{'ITF','PIL', 'KIM', 'NIN', 'NWS'})
+    metadataStr = 'm.cahill@aims.gov.au';
 elseif identifySite(site,{'NRSDAR'})
-    metadataStr = 'c.steinberg@aims.gov.au';
+    metadataStr = 'm.cahill@aims.gov.au';
 elseif identifySite(site,{'CAM', 'TAN'})
-    metadataStr = 'c.steinberg@aims.gov.au';
+    metadataStr = 'm.cahill@aims.gov.au';
 elseif identifySite(site,{'SCR'})
     metadataStr = 'adc@aims.gov.au, greg.ivey@uwa.edu.au';
 else
@@ -201,13 +190,13 @@ end
 end
 
 %%
-function metadataStr = getAIMSmetadata_institution_references(site)
+function metadataStr = getAIMSmetadata_institution_references(site, dbsite, isIMOS)
 
 if identifySite(site,{'SCR'})
     metadataStr = 'http://data.aims.gov.au, http://www.schmidtocean.org, http://www.imos.org.au/aodn.html';
-elseif identifySite(site,{'ITF','PIL', 'KIM', 'NIN', 'GBR', 'NRS'})
+elseif identifySite(site,{'ITF','PIL', 'KIM', 'NIN', 'GBR', 'NRS', 'NWS', 'TAN'})
     metadataStr = 'http://www.aims.gov.au/imosmoorings/, http://www.imos.org.au/aodn.html';
-elseif identifySite(site,{'CAM', 'TAN'})
+elseif identifySite(site,{'CAM'})
     metadataStr = 'http://www.aims.gov.au/imosmoorings/, http://www.imos.org.au/aodn.html';
 else
     metadataStr = 'http://data.aims.gov.au';
@@ -216,63 +205,72 @@ end
 end
 
 %%
-function metadataStr = getAIMSmetadata_acknowledgement(site)
-% v2.6.7+
-defaultStr = ['Any users of IMOS data are required to clearly acknowledge the source ',...
-    'of the material derived from IMOS in the format: "Data was sourced from Australia''s ',...
-    'Integrated Marine Observing System (IMOS) - IMOS is enabled by the National Collaborative ',...
-    'Research Infrastructure Strategy (NCRIS)."'];
+function metadataStr = getAIMSmetadata_acknowledgement(site, dbsite, isIMOS)
 
-if identifySite(site,{'GBR','NRSYON'})
-    metadataStr = [defaultStr,...
-        ' The support of the Department of Employment Economic ',...
-        'Development and Innovation of the Queensland State ',...
-        'Government is also acknowledged. The support of the ',...
-        'Tropical Marine Network (University of Sydney, Australian ',...
-        'Museum, University of Queensland and James Cook University) ',...
-        'on the GBR is also acknowledged.'];
-elseif identifySite(site,{'ITF','PIL', 'KIM', 'NIN'})
-    metadataStr = [defaultStr,...
-        ' The support of the Western Australian State Government is also acknowledged.'];
-elseif identifySite(site,{'NRSDAR'})
-    metadataStr = [defaultStr,...
-        ' The support of the Darwin Port Corporation is also acknowledged.'];
-elseif identifySite(site,{'CAM'})
-    metadataStr = [defaultStr,...
-        ' The collection of this data was funded by AIMS and IMOS and delivered ',...
-        'through the Queensland and Northern Australia Mooring sub-facility of the ',...
-        'Australian National Mooring Network operated by the Australian Institute of Marine Science.'];
-elseif identifySite(site,{'SCR'})
-    metadataStr = ['Any users of AIMS data are required to clearly acknowledge the source of the ',...
-        'material in this format: "Data was sourced from the ',...
-        'Australian Institute of Marine Science (AIMS). ',...
-        'The support of the University of Western Australia (UWA), the ',...
-        'Australian Research Council and the Schmidt ',...
-        'Ocean Institute (SOI) is also acknowledged."'];
-elseif identifySite(site,{'TAN100'})
-    strsplits = split(site,'-');
-    [yy,mm] = datevec(strsplits{2},'yymm');
-    % from TAN100-1907 onwards mooring is WA,AIMS,IMOS supported
-    if yy>=2019
+if isIMOS
+    % v2.6.7+
+    defaultStr = ['Any users of IMOS data are required to clearly acknowledge the source ',...
+        'of the material derived from IMOS in the format: "Data was sourced from Australia''s ',...
+        'Integrated Marine Observing System (IMOS) - IMOS is enabled by the National Collaborative ',...
+        'Research Infrastructure Strategy (NCRIS)."'];
+    
+    if identifySite(site,{'GBR','NRSYON'})
         metadataStr = [defaultStr,...
-            ' The collection of this data was funded by Western Australian State Government ',...
-            'Department of Jobs, Tourism, Science and Innovation; AIMS and IMOS and delivered ',...
+            ' The support of the Department of Employment Economic ',...
+            'Development and Innovation of the Queensland State ',...
+            'Government is also acknowledged. The support of the ',...
+            'Tropical Marine Network (University of Sydney, Australian ',...
+            'Museum, University of Queensland and James Cook University) ',...
+            'on the GBR is also acknowledged.'];
+    elseif identifySite(site,{'ITF','PIL', 'KIM', 'NIN'})
+        metadataStr = [defaultStr,...
+            ' The support of the Western Australian State Government is also acknowledged.'];
+    elseif identifySite(site,{'NRSDAR'})
+        metadataStr = [defaultStr,...
+            ' The support of the Darwin Port Corporation is also acknowledged.'];
+    elseif identifySite(site,{'CAM'})
+        metadataStr = [defaultStr,...
+            ' The collection of this data was funded by AIMS and IMOS and delivered ',...
             'through the Queensland and Northern Australia Mooring sub-facility of the ',...
             'Australian National Mooring Network operated by the Australian Institute of Marine Science.'];
+    elseif identifySite(site,{'TAN100'})
+        strsplits = split(site,'-');
+        [yy,mm] = datevec(strsplits{2},'yymm');
+        % from TAN100-1907 onwards mooring is WA,AIMS,IMOS supported
+        if yy>=2019
+            metadataStr = [defaultStr,...
+                ' The collection of this data was funded by Western Australian State Government ',...
+                'Department of Jobs, Tourism, Science and Innovation; AIMS and IMOS and delivered ',...
+                'through the Queensland and Northern Australia Mooring sub-facility of the ',...
+                'Australian National Mooring Network operated by the Australian Institute of Marine Science.'];
+        else
+            metadataStr = [defaultStr,...
+                ' The collection of this data was funded by AIMS and delivered ',...
+                'through the Queensland and Northern Australia Mooring sub-facility of the ',...
+                'Australian National Mooring Network operated by the Australian Institute of Marine Science.'];
+        end
     else
-        metadataStr = [defaultStr,...
-            ' The collection of this data was funded by AIMS and delivered ',...
-            'through the Queensland and Northern Australia Mooring sub-facility of the ',...
-            'Australian National Mooring Network operated by the Australian Institute of Marine Science.'];
+        metadataStr = defaultStr;
     end
-else
-    metadataStr = defaultStr;
+else % non IMOS
+    if identifySite(site,{'SCR'})
+        metadataStr = ['Any users of AIMS data are required to clearly acknowledge the source of the ',...
+            'material in this format: "Data was sourced from the ',...
+            'Australian Institute of Marine Science (AIMS). ',...
+            'The support of the University of Western Australia (UWA), the ',...
+            'Australian Research Council and the Schmidt ',...
+            'Ocean Institute (SOI) is also acknowledged."'];
+    else
+        metadataStr = ['Any users of AIMS data are required to clearly acknowledge the source of the ',...
+            'material in this format: "Data was sourced from the ',...
+            'Australian Institute of Marine Science (AIMS).'];
+    end
 end
 
 end
 
 %%
-function metadataStr = getAIMSmetadata_project_acknowledgement(site)
+function metadataStr = getAIMSmetadata_project_acknowledgement(site, dbsite, isIMOS)
 
 if identifySite(site,{'GBR','NRSYON'})
     metadataStr = ['The collection of this data was funded by IMOS ',...
@@ -317,13 +315,59 @@ end
 end
 
 %%
-function metadataStr = getAIMSmetadata_local_time_zone(site)
+function metadataStr = getAIMSmetadata_disclaimer(site, dbsite, isIMOS)
+
+if isIMOS
+    metadataStr = 'Data, products and services from IMOS are provided "as is" without any warranty as to fitness for a particular purpose.';
+else
+    metadataStr = 'Data, products and services from AIMS are provided "as is" without any warranty as to fitness for a particular purpose.';
+end
+
+end
+
+%%
+function metadataStr = getAIMSmetadata_citation(site, dbsite, isIMOS)
+
+if isIMOS
+    metadataStr = 'The citation in a list of references is: "IMOS [year-of-data-download], [Title], [data-access-URL], accessed [date-of-access].".';
+else
+    metadataStr = 'The citation in a list of references is: "AIMS [year-of-data-download], [Title], [data-access-URL], accessed [date-of-access].".';
+end
+
+end
+
+%%
+function metadataStr = getAIMSmetadata_data_centre(site, dbsite, isIMOS)
+
+if isIMOS
+    metadataStr = 'Australian Ocean Data Network (AODN)';
+else
+    metadataStr = 'AIMS Data Centre (ADC)';
+end
+
+end
+
+%%
+function metadataStr = getAIMSmetadata_data_centre_email(site, ~, isIMOS)
+
+if isIMOS
+    metadataStr = 'info@aodn.org.au';
+else
+    metadataStr = 'adc@aims.gov.au';
+end
+
+end
+
+%%
+function metadataStr = getAIMSmetadata_local_time_zone(site, dbsite, isIMOS)
+
+warning('Use of getAIMSmetadata_local_time_zone is problematic. Check that is covers your site.');
 
 if identifySite(site,{'GBR','NRSYON'})
     metadataStr = '+10';
 elseif identifySite(site,{'ITF','NRSDAR','NWSLYN'})
     metadataStr = '+9.5';
-elseif identifySite(site,{'PIL', 'KIM', 'NIN', 'SCR', 'TAN', 'CAM','NWSBRW','NWSROW','NWSBAR'})
+elseif identifySite(site,{'PIL', 'KIM', 'NIN', 'SCR', 'TAN', 'CAM','^NWS'})
     metadataStr = '+8';
 else
     metadataStr = '+10';
