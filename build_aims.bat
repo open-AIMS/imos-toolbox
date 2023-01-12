935be70cbf9f0e651a7c164314c02b2b6092325c
Simon Spagnol <s.spagnol@aims.gov.au>
2023-01-12 12:43:09 -0600

    Update current build script.

--- build_aims.bat
+++ build_aims.bat
@@ -1,9 +1,10 @@
 rem setlocal
 set CONDA=%USERPROFILE%\AppData\Local\Continuum\Anaconda3\condabin\conda
 call %CONDA% activate "%USERPROFILE%\AppData\Local\Continuum\Anaconda3"
+call %CONDA% activate IOOS
 
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_291
-set ANT_HOME=c:\opt\apache-ant-1.10.10
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_351
+set ANT_HOME=c:\opt\apache-ant-1.10.12
 set MATLAB_ROOT="C:\Program Files\MATLAB\R2018b"
 
 SET PATH=%ANT_HOME%\bin;%MATLAB_ROOT%\bin;%JAVA_HOME%\bin;%PATH%
